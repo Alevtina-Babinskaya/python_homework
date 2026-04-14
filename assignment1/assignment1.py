a4d28e1113b1f2c888d876b3d6958c601bb1cf73
Alevtina-Babinskaya <letters4alka@gmail.com>
2026-04-13 21:06:28 -0700

    after review changes

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -140,3 +140,4 @@ def pig_latin (string):
         else:            
             pig_latin_words[pig_latin_words.index(pl_word)] = pl_word + "ay"
     return " ".join(pig_latin_words)
+pig_latin("the quick brown fox")
